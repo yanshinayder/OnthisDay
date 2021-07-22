8a7786f7c806c1c46a266154c982753543c28438
Yan <shinayderepcar@outlook.com>
2021-07-21 22:47:22 -0300

    Adicionado arquivo services.py que vai gerenciar o arquivo a ser aberto

---
+++ services.py
@@ -0,0 +1,19 @@
+from typing import Dict
+import json as _json
+
+
+def get_all_events() -> Dict:
+    with open("events.json") as events_file:
+        data = _json.load(events_file)
+
+    return data
+
+def get_month_events(month: str) -> Dict:
+    events = get_all_events()
+    try:
+        month_events = events[month]
+        return month_events
+    except KeyError:
+        return "favor digitar um mês válido"
+
+
